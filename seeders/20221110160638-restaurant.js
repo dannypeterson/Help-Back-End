4473eed6220e6f1ef0ee0135e611c766c32da8d7
Danny <dannypeterson28@gmail.com>
2022-11-10 12:12:07 -0500

    restaurant data seeded

--- seeders/20221110160638-restaurant.js
+++ seeders/20221110160638-restaurant.js
@@ -35,29 +35,54 @@ module.exports = {
           address: '140 Boylston St, Boston, MA, 02116',
           category: 'Seafood',
           img: 'https://media-cdn.tripadvisor.com/media/photo-s/0d/b3/f4/27/awesome-concept.jpg',
-          updatedAt: new Date()
-        },
-        {
-          name: 'Nu Burger',
-          address: '401 Park Dr, Boston, MA 02215',
-          category: 'American',
-          img: 'https://media.foodnetwork.ca/imageserve/wp-content/uploads/sites/6/2015/03/Exterior2-e1425495506573/x.jpg',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
-          name: 'Nu Burger',
-          address: '401 Park Dr, Boston, MA 02215',
-          category: 'American',
-          img: 'https://media.foodnetwork.ca/imageserve/wp-content/uploads/sites/6/2015/03/Exterior2-e1425495506573/x.jpg',
+          name: 'Union Oyster House',
+          address: '41 Union St, Boston, MA, 02108',
+          category: 'Seafood',
+          img: 'https://assets.simpleviewinc.com/simpleview/image/upload/crm/boston/UOHExterior_57E89CB1-5056-A36A-06EEC25D6A517C62-57e89bf15056a36_57e89cf2-5056-a36a-06c410cc896f6735.png',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
-          name: 'Nu Burger',
-          address: '401 Park Dr, Boston, MA 02215',
+          name: 'The Salty Pig',
+          address: '130 Dartmouth St, Boston, MA, 02116',
           category: 'American',
-          img: 'https://media.foodnetwork.ca/imageserve/wp-content/uploads/sites/6/2015/03/Exterior2-e1425495506573/x.jpg',
+          img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffastly.4sqi.net%2Fimg%2Fgeneral%2F600x600%2F1084242_0_5I54xsTJq0nP77LgB79Y8NyziUbfOOMwx-OOH74Js.jpg&f=1&nofb=1&ipt=44829f5c0b78deda7c4dc0b4968a0191701bb90e7e690657942fc14327a790dd&ipo=images',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Umami Omaskse',
+          address: ' 2372 Massachusetts Ave, Cambridge, MA, 02140',
+          category: 'Asian',
+          img: 'https://infatuation.imgix.net/media/images/reviews/umami/banners/1573576298.77.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: ' Wen’s Yunnan Noodle and Ramen',
+          address: '247 Newbury St, Boston, MA 02116',
+          category: 'Asian',
+          img: 'https://media-cdn.tripadvisor.com/media/photo-m/1280/22/0c/b9/8b/wen-s-noodles.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Shake Shack',
+          address: '234 Newbury St, Boston, MA 02116',
+          category: 'FastFood',
+          img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fc6%2F58%2F90%2Fc658900c7b5fc9f1f8cc649720954a3a.jpg&f=1&nofb=1&ipt=619e02fe957756c0730495374e882343cf20009f76017acd618a08f4f0f9a042&ipo=images',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Krasi',
+          address: '48 Gloucester Street, Boston, MA, 02115',
+          category: 'Mediterranean',
+          img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frcon.gr%2Fwp-content%2Fuploads%2F2020%2F12%2FRCON_Design_Construction_Krasi_5-640x483.jpg&f=1&nofb=1&ipt=91884105c3308a7991cfd5256c8387d93388cd8c051388335f049b35a5aebe98&ipo=images',
           createdAt: new Date(),
           updatedAt: new Date()
         }
--- seeders/20221110160715-review.js
+++ seeders/20221110160715-review.js
@@ -3,15 +3,100 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('reviews')
+    await queryInterface.bulkInsert('reviews', [
+      {
+        dish: 'Umami Truffle Burger',
+        description: 'sheeeeesh this was a good burger. The truffle? Perfect.',
+        rating: 5,
+        img: 'https://i.imgur.com/TDI5eVO.png',
+        restaurantId: '1',
+        userId: '1'
+      },
+      {
+        dish: 'Half Pound Maine Lobster Roll',
+        description:
+          'Most delicious lobster roll in Boston! The freshest I have had in town, well worth the price.',
+        rating: 5,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photosV2/9f04a886-b55f-4313-ad2b-1ed67e4f3760-retina-large.JPG',
+        restaurantId: '3',
+        userId: '2'
+      },
+      {
+        dish: 'Salty Pig Pizza',
+        description:
+          'This was a weird dish. Salty pig parts on pizza? And the waiter spit on the pizza before serving it???',
+        rating: 2,
+        img: 'blob:https://imgur.com/ea0dd1d8-f464-4b77-ac52-220f85a1127b',
+        restaurantId: '6',
+        userId: '3'
+      },
+      {
+        dish: 'Clam Chowder',
+        description:
+          'Delicious hot cup of corn chowder. It did taste a little oversalted and I could have used more oyster crackers. All in all, a tasty dish at a historic restaurant.',
+        rating: 4,
+        img: 'https://media-cdn.tripadvisor.com/media/photo-s/16/ad/1e/25/photo6jpg.jpg',
+        restaurantId: '5',
+        userId: '4'
+      },
+      {
+        dish: 'Fried Catfish Basket',
+        description:
+          'I had to try this unique catfish dish! It was done well but I wish I had tried a more signature dish at this restaurant.',
+        rating: 3,
+        img: 'https://cloverstatic.com/menu-assets/items/2QZ6KPWCK0EYE_576x576.jpeg',
+        restaurantId: '4',
+        userId: '5'
+      },
+      {
+        dish: 'Hot Ones Burger',
+        description:
+          "McDonald's could never. Shake Shack has the superior squiggly fries. In-n-out still clear tho. Sean Evans the goat.",
+        rating: 5,
+        img: 'https://d2luv1saso99wi.cloudfront.net/2022_Hot-Ones_App-Web-Menu_1500x920_Burger_lg1661364018.jpeg',
+        restaurantId: '9',
+        userId: '6'
+      },
+      {
+        dish: 'Stifado',
+        description:
+          'These braised short ribs did not disappoint! I highly recommend pairing this with an earthy red wine. I wish the meat was cooked a little more well done.',
+        rating: 4,
+        img: 'https://www.danishfoodlovers.com/wp-content/uploads/2022/07/Greek-beef-stifado-5.jpg',
+        restaurantId: '10',
+        userId: '7'
+      },
+      {
+        dish: 'Crab-Crusted Haddock',
+        description:
+          'I enjoyed this interesting dish! Crab and haddock, how could you go wrong? So happy I came to this restaurant while visiting Boston.',
+        rating: 4,
+        img: 'https://citypierseafood.com/media/catalog/product/c/p/cpfcod1000-cod-loin-plated-detail_1.png',
+        restaurantId: '2',
+        userId: '8'
+      },
+      {
+        dish: 'A5 Wagyu Beef Foie Gras Don',
+        description:
+          'Do I need to say anything more? This wagyu cow was still mooing fresh from Japan. Highly recommend.',
+        rating: 5,
+        img: 'https://i.imgur.com/omkmskd.png',
+        restaurantId: '7',
+        userId: '9'
+      },
+      {
+        dish: 'Sho-yu Ramen',
+        description:
+          'wen yunnan. I enjoyed a hot bowl of ramen while shopping on Newbury Street. I wish the prices had been lower so I could have customized my dish more. I would come back again.',
+        rating: 3,
+        img: 'https://img.cdn4dd.com/cdn-cgi/image/fit=cover,width=600,height=400,format=auto,quality=80/https://doordash-static.s3.amazonaws.com/media/store/header/9f5f69b4-00cb-4800-8474-17958657b726.jpg',
+        restaurantId: '8',
+        userId: '10'
+      }
+    ])
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('reviews', null, {})
   }
 }
