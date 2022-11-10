5b6b7c5719b7f26b7ef9c35b25aab56bcbb4e886
Danny <dannypeterson28@gmail.com>
2022-11-10 10:47:12 -0500

    created db and models

---
+++ migrations/20221110154014-create-review.js
@@ -0,0 +1,43 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      dish: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      img: {
+        type: Sequelize.STRING
+      },
+      restauarantId: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('reviews')
+  }
+}
---
+++ models/review.js
@@ -0,0 +1,30 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Review.init(
+    {
+      dish: DataTypes.STRING,
+      description: DataTypes.STRING,
+      rating: DataTypes.INTEGER,
+      img: DataTypes.STRING,
+      restauarantId: DataTypes.STRING,
+      userId: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'Review',
+      tableName: 'reviews'
+    }
+  )
+  return Review
+}
