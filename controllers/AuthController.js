634600941173a4a43f854685aa2ba66cc5b38198
Maciej Parol <maciejparol333@gmail.com>
2022-11-14 09:20:30 -0500

    added login auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -12,12 +12,28 @@ const Register = async (req, res) => {
   }
 }
 
-// const Login = async (req, res) => {
-//   try {
-//   } catch (error) {
-//     throw error
-//   }
-// }
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { email: req.body.email },
+      raw: true
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(user.passwordDigest, req.body.password))
+    ) {
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    throw error
+  }
+}
 
 // const CreateUser = async (req, res) => {
 //   try {
@@ -47,6 +63,6 @@ module.exports = {
   // CreateUser,
   FindUserById,
   GetAllUsers,
-  // Login,
+  Login,
   Register
 }
