6ce2225e4971dc2996cb6723a971cc9654849442
Danny <dannypeterson28@gmail.com>
2022-11-17 11:28:25 -0500

    added express json

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ msg: `Server Works!` }))
 app.use('/user', AuthRouter)
